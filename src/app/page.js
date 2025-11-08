67f3f6d396cc2e0f82f3f695d493b73761240a0c
Petrus Hendrick Geyu <petrusgeyu34.com>
2025-11-08 21:13:33 +0700

    redirect dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,21 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/dashboard"); // langsung arahkan ke dashboard
+  }, [router]);
+
   return (
-   <>
     <main className="flex flex-col items-center justify-center min-h-screen text-center">
       <h1 className="text-4xl font-bold text-green-600">Fenya E-Ticket</h1>
-      <p className="mt-2 text-gray-600">Pesan tiket taman wisata dengan mudah, bahkan tanpa internet.</p>
-      <a href="/login" className="mt-6 bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600">
-        Masuk Sekarang
-      </a>
+      <p className="mt-2 text-gray-600">
+        Mengarahkan ke dashboard...
+      </p>
     </main>
-   </>
   );
 }
