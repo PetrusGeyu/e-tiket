1e74fe75389624d372bfff10e9c55c8a7a5892fa
Petrus Hendrick Geyu <petrusgeyu34.com>
2025-11-10 17:40:37 +0700

    api edit

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://lapakjarubackend.xyz/api",
-  timeout: 10000,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://lapakjarubackend.xyz/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
+
+
